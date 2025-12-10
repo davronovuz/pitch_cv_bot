d91a31e01c1ae4fd30a37fb26f182b8159a243a9
davronovuz <davronovtatu@gmail.com>
2025-12-10 12:53:08 +0500

    sasassas

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -4,7 +4,6 @@ from . import plan_admin
 from . import user_handlers
 from . import reklama
 from . import statistika_admin
-from . import channel_subscription
 from . import admin_panel
 from . import help
 
