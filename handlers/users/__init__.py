893bc4810f53ff56b40ed89ec464a0c4793bff99
davronovuz <davronovtatu@gmail.com>
2025-12-15 14:17:08 +0500

    S wew

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,10 +1,11 @@
+from . import mahalla
 from . import admin_free_pptx
 from . import course_work_handler
 from . import plan_admin
 from . import user_handlers
 from . import reklama
 from . import statistika_admin
-from . import mahalla
+
 
 
 
