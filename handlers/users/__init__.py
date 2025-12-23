472943b94ae621d521becf34d9f3d0ebac27b985
davronovuz <davronovtatu@gmail.com>
2025-12-23 12:59:51 +0500

    sas as asassaas

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,6 +1,6 @@
 from . import mahalla
 from . import admin_free_pptx
-from . import course_work_handler
+from . import course_work_handler1
 from . import plan_admin
 from . import user_handlers
 from . import reklama
